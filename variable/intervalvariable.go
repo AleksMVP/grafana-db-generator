5827b55e2eaad94ca427d74a68ca9a4adfdef1d7
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2021-08-13 12:15:34 +0300

    Add interval variable

---
+++ variable/intervalvariable.go
@@ -0,0 +1,81 @@
+package variable
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aleksmvp/grafanagenerator/utils"
+)
+
+type intervalVariableTemplate struct {
+	Current string
+	Options string 
+	Query   string
+}
+
+type IntervalVariable struct {
+	data intervalVariableTemplate
+}
+
+func NewIntervalVariable(intervals []string) IntervalVariable {
+	opts := []option{}
+	cur  := option{
+		Selected: false,
+		Text:     intervals[0],
+		Value:    intervals[0],
+	}
+
+	query   := ""
+
+	for _, value := range intervals {
+		opts = append(opts, option{
+			Selected: false,
+			Text:     value,
+			Value:    value,
+		})
+
+		query += fmt.Sprintf("%v,", value)
+	}
+
+	opts[0].Selected = true
+
+	query = utils.CutLastElement(query)
+
+	optsJson, _ := json.Marshal(opts)
+	curJson, _  := json.Marshal(cur)
+
+	return IntervalVariable{
+		data: intervalVariableTemplate{
+			Current: string(curJson),
+			Options: string(optsJson),
+			Query:   query,
+		},
+	}
+}
+
+func (instance *IntervalVariable) Draw()(variable string, err error) {
+	variable, err = utils.ExecuteTemplate(INTERVAL_VAR_TEMPLATE, instance.data)
+
+	return variable, err
+}
+
+const (
+	INTERVAL_VAR_TEMPLATE = `
+{
+	"auto": false,
+	"auto_count": 30,
+	"auto_min": "10s",
+	"current": {{ .Current }},
+	"description": null,
+	"error": null,
+	"hide": 0,
+	"label": null,
+	"name": "scape",
+	"options": {{ .Options }},
+	"query": "{{ .Query }}",
+	"queryValue": "",
+	"refresh": 2,
+	"skipUrlSync": false,
+	"type": "interval"
+}`
+)
