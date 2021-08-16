4684b7009943590b51d62470a2896b1349dea9e4
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2021-08-16 16:34:41 +0300

    Add stat chat

---
+++ chart/statchart.go
@@ -0,0 +1,108 @@
+package chart
+
+import (
+	"github.com/aleksmvp/grafanagenerator/models"
+	"github.com/aleksmvp/grafanagenerator/utils"
+)
+
+type StatChart struct {
+	data chartTemplate
+}
+
+func NewStatChart(title string, height, width int, targets []models.ChartTarget) (Chart) {
+	return Chart{
+		data: chartTemplate{
+			Height:  height,
+			Width:   width,
+			Title:   title,
+			Targets: targets,
+		},
+	}
+}
+
+func (instance *StatChart) Draw(x, y, ID int) (chart string, nextX, nextY, height int, err error) {
+	instance.data.X  = x
+	instance.data.Y  = y
+	instance.data.ID = ID
+
+	chart, err = utils.ExecuteTemplate(STAT_CHART_TEMPLATE, instance.data)
+	if err != nil {
+		return
+	}
+
+	nextY  = y
+	nextX  = x + instance.data.Width
+	height = instance.data.Height
+
+	return 
+}
+
+const (
+	STAT_CHART_TEMPLATE = `{
+		"datasource": null,
+		"description": "",
+		"fieldConfig": {
+			"defaults": {
+				"color": {
+					"mode": "thresholds"
+				},
+				"mappings": [],
+				"thresholds": {
+					"mode": "absolute",
+					"steps": [
+						{
+							"color": "red",
+							"value": null
+						},
+						{
+							"color": "green",
+							"value": 0
+						}
+					]
+				}
+			},
+			"overrides": []
+		},
+		"gridPos": {
+			"h": {{ .Height }},
+			"w": {{ .Width }},
+			"x": {{ .X }},
+			"y": {{ .Y }}
+		},
+		"id": {{ .ID }},
+		"options": {
+			"colorMode": "value",
+			"graphMode": "none",
+			"justifyMode": "center",
+			"orientation": "auto",
+			"reduceOptions": {
+				"calcs": [
+					"lastNotNull"
+				],
+				"fields": "",
+				"values": false
+			},
+			"text": {},
+			"textMode": "auto"
+		},
+		"pluginVersion": "7.5.2",
+		"targets": [
+			{{ range $index, $item := .Targets }}
+				{{ if ne $index 0 }}
+				  ,
+				{{ end }}
+				{
+				  "exemplar": true,
+				  "expr": "{{ .Expr }}",
+				  "interval": "",
+				  "legendFormat": "{{ .Legend }}",
+				  "refId": "{{ .RefId }}"
+				}
+			{{ end }}
+		],
+		"timeFrom": null,
+		"timeShift": null,
+		"title": "{{ .Title }}",
+		"type": "stat"
+	}`
+)
---
+++ ui_test.go
@@ -0,0 +1,28 @@
+package grafanagenerator
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aleksmvp/grafanagenerator/chart"
+	"github.com/aleksmvp/grafanagenerator/models"
+	"github.com/aleksmvp/grafanagenerator/row"
+	"github.com/aleksmvp/grafanagenerator/variable"
+)
+
+func TestCreateConfig(t *testing.T) {
+	row := row.NewRow("kk")
+	char := chart.NewChart("kdkd", 1, 3, []models.ChartTarget{})
+	cff   := chart.NewStatChart("kkfdj", 1, 3, []models.ChartTarget{})
+	variable.NewConstVariable("k3k", "k3k")
+	variable.NewCustomVariable("kek", map[string]string{})
+
+	v := variable.NewIntervalVariable("kek", []string{"1m","10m","30m","1h","6h","12h","1d","7d","14d","30d"})
+	fmt.Println(v.Draw())
+	// db := dashboard.NewDashboard("kek")	
+
+	// fmt.Println(db)
+	fmt.Println(row)
+	fmt.Println(char)
+	fmt.Println(cff)
+}	
